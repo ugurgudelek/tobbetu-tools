f5dfb918e2dbd88bd36194c8625a36f22d22d078
Uğur Güdelek <ugurgudelek@gmail.com>
2019-05-04 12:21:16 +0300

    04.05.19 minor changes on URL

--- ical/src/ical.py
+++ ical/src/ical.py
@@ -6,28 +6,28 @@ import pytz
 
 # URL = 'http://kayit.etu.edu.tr/butunleme_gozetmenlikleri.php'
 # URL = 'http://kayit.etu.edu.tr/ara_sinav_programi.php'
-URL = 'http://kayit.etu.edu.tr/Ders/donem_sonu_sinav_gozetmenlikleri.php'
-gozetmen_name = 'MEHMET UĞUR GÜDELEK'
+URL = 'http://kayit.etu.edu.tr/rapor/final_sinav_programi.php'
+# gozetmen_name = 'MEHMET UĞUR GÜDELEK'
 # gozetmen_name = 'MUHAMMED YILMAZ'
-# gozetmen_name = 'ZEYNEP TÜFEKCİ'
+gozetmen_name = 'ZEYNEP TÜFEKCİ'
 # gozetmen_name = 'CANSIN BAYRAK'
 # gozetmen_name = 'IŞIKSU EKŞİOĞLU'
+# gozetmen_name = 'UĞUR ŞAHİN'
 
-gozetmenlik_df = pd.read_html(URL, header=0)[0]
+gozetmenlik_df = pd.read_html(URL, header=0, encoding='utf-8')[0]
 
-gozetmenlik_df = gozetmenlik_df.loc[gozetmenlik_df['GÖZETMEN'] == gozetmen_name].reset_index(drop=True)
-
+gozetmenlik_df = gozetmenlik_df.loc[gozetmenlik_df['GOZETMEN'] == gozetmen_name].reset_index(drop=True)
 
 gozetmenlik_df['HSTART'] = gozetmenlik_df['SAAT'].apply(lambda x:x.split('-')[0])
 gozetmenlik_df['HEND'] = gozetmenlik_df['SAAT'].apply(lambda x:x.split('-')[1])
-gozetmenlik_df['DTSTART'] = gozetmenlik_df.apply(lambda row: row['TARİH'] + '-' + row['HSTART'], axis=1)
-gozetmenlik_df['DTEND'] = gozetmenlik_df.apply(lambda row: row['TARİH'] + '-' + row['HEND'], axis=1)
+gozetmenlik_df['DTSTART'] = gozetmenlik_df.apply(lambda row: row['TARIH'] + '-' + row['HSTART'], axis=1)
+gozetmenlik_df['DTEND'] = gozetmenlik_df.apply(lambda row: row['TARIH'] + '-' + row['HEND'], axis=1)
 
-gozetmenlik_df['DTSTART'] = gozetmenlik_df['DTSTART'].apply(lambda x: pd.to_datetime(x, format='%d.%m.%Y-%H:%M'))
-gozetmenlik_df['DTEND'] = gozetmenlik_df['DTEND'].apply(lambda x: pd.to_datetime(x, format='%d.%m.%Y-%H:%M'))
+gozetmenlik_df['DTSTART'] = gozetmenlik_df['DTSTART'].apply(lambda x: pd.to_datetime(x, format='%d/%m/%Y-%H:%M'))
+gozetmenlik_df['DTEND'] = gozetmenlik_df['DTEND'].apply(lambda x: pd.to_datetime(x, format='%d/%m/%Y-%H:%M'))
 gozetmenlik_df['DTSTART'] = gozetmenlik_df.loc[:,'DTSTART'].dt.tz_localize(pytz.timezone('Europe/Istanbul'))
 gozetmenlik_df['DTEND'] = gozetmenlik_df.loc[:,'DTEND'].dt.tz_localize(pytz.timezone('Europe/Istanbul'))
-gozetmenlik_df = gozetmenlik_df.drop(['TARİH', 'GÜN', 'SAAT', 'HSTART', 'HEND'], axis=1)
+gozetmenlik_df = gozetmenlik_df.drop(['TARIH', 'GUN', 'SAAT', 'HSTART', 'HEND'], axis=1)
 
 
 ical = icalendar.Calendar()
@@ -38,7 +38,7 @@ for idx,row in gozetmenlik_df.iterrows():
     event.add('summary', 'Gozetmenlik - {}'.format(row['DERS ADI']))
     event.add('dtstart', row['DTSTART'].to_pydatetime())
     event.add('dtend', row['DTEND'].to_pydatetime())
-    event.add('location', row['DERSLİK'])
+    event.add('location', row['DERSLIK'])
     ical.add_component(event)
 
 
